f887fd142d14ba22f9b391693571866e1f7ea8a4
YuriFA <yurik1452@mail.ru>
2016-10-15 16:50:47 +0300

    init5

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
-$(document).ready(function(){
-    $('#check_all_tasks').on('click', function(e){
-        var checks = $(this).parents('.todo').find('ul.todo_list .custom_checkbox');
+$(document).ready(function() {
+    $('#check_all_tasks').on('click', function(e) {
+        var checks = $(this).parents('.todo').find('ul.todo_list .custom_checkbox'),
+            is_check = $(this).is(':checked');
         checks.each(function(){
-            if(!$(this).is(':checked')) {
+            if(( is_check && !$(this).is(':checked') ) || ( !is_check && $(this).is(':checked') ) ) {
                 $(this).trigger('click');
             }
         });
