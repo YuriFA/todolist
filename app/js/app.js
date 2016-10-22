4031f0baafcde4404f2d95474f44322cbef0e996
YuriFA <yurik1452@mail.ru>
2016-10-22 19:51:32 +0300

    'ng1'

--- app/js/app.js
+++ app/js/app.js
@@ -18,20 +18,23 @@ todolistApp.factory('Lists', [
 ]);
 
 todolistApp.controller('TodoListCtrl',[
-  '$scope','$http', '$location', 'Lists',
+  '$scope','$document', '$location', 'Lists',
   function($scope, $http, $location, Lists) {
 
     Lists.query({listId: 'lists'}, function(data) {
       $scope.lists = data;
     });
 
+    $scope.toggle = [];
+    $scope.openNewTaskForm = function(id){
+      $scope.toggle[id] = $scope.toggle[id] == 'show'? '' : 'show';
+    };
+
+    $scope.addTask = function() {
+    };
     //Phone.query(params, successcb, errorcb)
-
     //Phone.get(params, successcb, errorcb)
-
     //Phone.save(params, payloadData, successcb, errorcb)
-
     //Phone.delete(params, successcb, errorcb)
-
   }
 ]);
