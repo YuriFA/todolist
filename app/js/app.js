48dd0297a8d13dac757104164c0eeadaab99c28a
YuriFA <yurik1452@mail.ru>
2016-10-23 19:15:34 +0300

    'ng2'

--- app/js/app.js
+++ app/js/app.js
@@ -1,40 +1,70 @@
 'use strict';
 
-/* Controllers */
+/* Module */
 var todolistApp = angular.module('todolistApp', ['ngResource']);
+todolistApp.constant('MONGOLAB_CONFIG',{API_KEY:'R5yAQrpwxPxNszWhqmMhqbqgbzAyyQUQ', DB_NAME:'todo'});
 
-/* Factory */
+/* Lists Factory */
 todolistApp.factory('Lists', [
   '$resource', function($resource) {
     return $resource('lists/:listId.:format', {
       listId: 'lists',
       format: 'json',
     }, {
-      // action: {method: <?>, params: <?>, isArray: <?>, ...}
       update: {method: 'PUT', params: {listId: '@list'}, isArray: true}
     });
-    //Phone.update(params, successcb, errorcb);
+  }
+]);
+
+/* ToDos Factory */
+todolistApp.factory('ToDos', [
+  '$resource', function($resource) {
+    return $resource('lists/list_:listId.:format', {
+      listId: '@list_id',
+      format: 'json',
+    }, {
+      update: {
+        method: 'PUT'
+      }
+    });
   }
 ]);
 
 todolistApp.controller('TodoListCtrl',[
-  '$scope','$document', '$location', 'Lists',
-  function($scope, $http, $location, Lists) {
+  '$scope','$document', '$location', 'Lists', 'ToDos',
+  function($scope, $http, $location, Lists, ToDos) {
 
-    Lists.query({listId: 'lists'}, function(data) {
+    // var postData = {
+    //   "id": 42,
+    //   "title": "The Hitchhiker's Guide to the Galaxy",
+    //   "checked": false
+    // }
+    // ToDos.save({}, postData);
+
+    Lists.query(function(data) {
       $scope.lists = data;
+      angular.forEach(data, function(list, i) {
+        $scope.lists[i].tasks = ToDos.query({listId: list.id});
+      });
     });
+
+    $scope.newTodo = {};
+    $scope.addTask = function(list_id) {
+      var list_tasks = $scope.lists[list_id].tasks;
+      $scope.new_task = new ToDos({
+        id: list_tasks.length + 1,
+        text: $scope.newTodo[list_id].text,
+        checked: false
+      });
+      list_tasks.push($scope.new_task);
+      ToDos.update({listId: list_id}, list_tasks);
+      $scope.newTodo[list_id].text = '';
+    };
 
     $scope.toggle = [];
     $scope.openNewTaskForm = function(id){
       $scope.toggle[id] = $scope.toggle[id] == 'show'? '' : 'show';
     };
 
-    $scope.addTask = function() {
-    };
-    //Phone.query(params, successcb, errorcb)
-    //Phone.get(params, successcb, errorcb)
-    //Phone.save(params, payloadData, successcb, errorcb)
-    //Phone.delete(params, successcb, errorcb)
   }
 ]);
