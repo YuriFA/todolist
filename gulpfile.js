e7a777d3c98da79268d023788b331225292dfd35
YuriFA <yurik1452@mail.ru>
2016-10-25 16:57:02 +0300

    change styles

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
 var gulp = require('gulp');
 var server = require('gulp-server-livereload');
-
+var sass = require('gulp-sass');
+var prefix = require('gulp-autoprefixer');
+var useref = require('gulp-useref');
+var gulpif = require('gulp-if');
+var uglify = require('gulp-uglify');
+var minifyCss = require('gulp-csso');
 
 gulp.task('server', function() {
     gulp.src('app')
@@ -10,5 +15,25 @@ gulp.task('server', function() {
         }));
 });
 
+gulp.task('style', function() {
+    return gulp.src('app/sass/**/*.sass')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(prefix({
+            browsers: ['last 15 versions']
+        }))
+        .pipe(gulp.dest('app/css'));
+});
 
-gulp.task('default', ['server']);
+gulp.task('watch', function() {
+    gulp.watch('app/sass/**/*.sass', ['style']);
+});
+
+gulp.task('build', function() {
+    return gulp.src('app/*.html')
+        .pipe(useref())
+        .pipe(gulpif('*.js', uglify()))
+        .pipe(gulpif('*.css', minifyCss()))
+        .pipe(gulp.dest('public'));
+});
+
+gulp.task('default', ['server', 'watch']);
